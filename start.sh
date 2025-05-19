d3f329e4c149ab51bf8f36fb9703e1ff3798d189
Velvet <solusoperandi@protonmail.ch>
2025-05-19 14:29:48 -0400

    add start shell script file

---
+++ start.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+set -e
+source /home/velvet/Documents/Projects/Development/mirrorfield/venv/bin/activate
+cd /home/velvet/Documents/Projects/Development
+/home/velvet/Documents/Projects/Development/mirrorfield/venv/bin/python3 -m mirrorfield
